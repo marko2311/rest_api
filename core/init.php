5ec8810b0ad7a20460a397bce051965649d77195
unknown <marcin.sapiela11@gmail.com>
2020-10-08 20:20:24 +0200

    Defined path names in init.php file

--- core/init.php
+++ core/init.php
@@ -1,7 +1,16 @@
 <?php
 
-define('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
-define('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'htdocs'. DS . 'gog_task');
+defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'htdocs'. DS . 'gog_task');
+defined('INCLUDE_PATH') ? null : define('INCLUDE_PATH', SITE_ROOT . DS . 'includes');
+defined('CORE_PATH') ? null : define('CORE_PATH', SITE_ROOT . DS . 'core');
+
+//load config
+require_once(INCLUDE_PATH . DS . "config.php");
+
+//core classes
+//require_once(CORE_PATH . DS . "post.php");
+
 class init
 {
 
