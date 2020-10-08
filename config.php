7c984e64f7d7a0738b74b1725797b1e9ae8c0639
unknown <marcin.sapiela11@gmail.com>
2020-10-08 19:25:37 +0200

    addin attributes to db connection

--- config.php
+++ config.php
@@ -5,3 +5,7 @@ $db_password = "";
 $db_name     = "games";
 
 $db = new PDO('mysql:host=127.0.0.1;dbname='.$db_name.';charset=utf8', $db_user, $db_password);
+
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
