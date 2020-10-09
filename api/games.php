930cd97edcc9765259d8146b7ab305b167fe99e9
unknown <marcin.sapiela11@gmail.com>
2020-10-09 13:37:56 +0200

    add paging feature

--- api/games.php
+++ api/games.php
@@ -10,19 +10,24 @@ $game = new Game($db, $requested_id);
 
 $result = $game->read();
 $num = $result->rowCount();
+// number of pages
+$pages = ceil($num / 3);
 
 if($num > 0){
     $game_array = array();
     $game_array['data'] = array();
-
+    $page_number = 1;
     while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        isset($game_array['data'][$page_number]) ? null : $game_array['data'][$page_number] = array();
         extract($row);
         $game_item = array(
             'id'    => $id,
             'name'  => $name,
             'price' => $price
         );
-        array_push($game_array['data'], $game_item);
+        array_push($game_array['data'][$page_number], $game_item);
+        if(count($game_array['data'][$page_number]) == 3)
+            $page_number++;
     }
     echo json_encode($game_array);
 } else {
