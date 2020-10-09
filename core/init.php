73e6bdf62dd5e21e3554b5aedfb0c33ad497c731
unknown <marcin.sapiela11@gmail.com>
2020-10-09 13:45:35 +0200

    add paging feature

--- core/init.php
+++ core/init.php
@@ -1,7 +1,7 @@
 <?php
 
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
-defined('SITE_ROOT') ? null : define('SITE_ROOT', DS.'xampp' . DS . 'htdocs'. DS . 'gog_task');
+defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'xampp' . DS . 'htdocs'. DS . 'gog_task');
 defined('INCLUDE_PATH') ? null : define('INCLUDE_PATH', SITE_ROOT . DS . 'includes');
 defined('CORE_PATH') ? null : define('CORE_PATH', SITE_ROOT . DS . 'core');
 
